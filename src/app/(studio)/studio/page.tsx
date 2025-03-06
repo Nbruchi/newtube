9cc111d72b1faba4f476f01b702ce29f1918bf59
Nbruchi <nkundabagenzibruce@gmail.com>
2025-03-06 21:35:33 +0200

    Created all db entities

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/views/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-const Page = () => {
-    return <div>Studio</div>;
+const Page = async () => {
+    void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
+
+    return (
+        <HydrateClient>
+            <StudioView />
+        </HydrateClient>
+    );
 };
 
 export default Page;
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,12 @@
 import { createTRPCRouter } from "../init";
+import { studioRouter } from "@/modules/studio/server/procedures";
 import { categoriesRouter } from "@/modules/auth/categories/server/procedures";
+import { videosRouter } from "@/modules/videos/server/procedures";
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
+    studio: studioRouter,
+    videos: videosRouter,
 });
 
 export type AppRouter = typeof appRouter;
