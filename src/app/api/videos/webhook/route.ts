ff80a316301822e8a5b93f320a73daec817a90d9
Nbruchi <nkundabagenzibruce@gmail.com>
2025-03-08 08:30:18 +0200

    Add video thumbnail

---
+++ src/app/api/videos/webhook/route.ts
@@ -0,0 +1,96 @@
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
+import {
+    VideoAssetCreatedWebhookEvent,
+    VideoAssetErroredWebhookEvent,
+    VideoAssetReadyWebhookEvent,
+    VideoAssetTrackReadyWebhookEvent,
+} from "@mux/mux-node/resources/webhooks";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+
+const SIGNING_SECRET = process.env.MUX_SIGNING_SECRET!;
+
+type WebhookEvent =
+    | VideoAssetCreatedWebhookEvent
+    | VideoAssetErroredWebhookEvent
+    | VideoAssetReadyWebhookEvent
+    | VideoAssetTrackReadyWebhookEvent;
+
+export const POST = async (request: Request) => {
+    if (!SIGNING_SECRET) {
+        throw new Error(`MUX_SIGNING_SECRET is not set`);
+    }
+
+    const headersPayload = await headers();
+    const muxSignature = headersPayload.get("mux-signature");
+
+    if (!muxSignature) {
+        return new Response(`No signature found`, { status: 401 });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    mux.webhooks.verifySignature(
+        body,
+        {
+            "mux-signature": muxSignature,
+        },
+        SIGNING_SECRET
+    );
+
+    switch (payload.type as WebhookEvent["type"]) {
+        case "video.asset.created": {
+            const data = payload.data as VideoAssetCreatedWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("No upload ID found", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxAssetId: data.id,
+                    muxStatus: data.status,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.ready": {
+            const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+            const playbackId = data.playback_ids?.[0].id;
+
+            if (!data.upload_id) {
+                return new Response(`Missing upload ID`, { status: 400 });
+            }
+
+            if (!playbackId) {
+                return new Response(`Missing playback ID`, { status: 400 });
+            }
+
+            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.png`;
+            const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
+            const duration = data.duration
+                ? Math.round(data.duration * 100)
+                : 0;
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                    muxPlaybackId: playbackId,
+                    muxAssetId: data.id,
+                    thumbnailUrl,
+                    previewUrl,
+                    duration,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+    }
+
+    return new Response(`Webhook Received`, { status: 200 });
+};
--- src/modules/studio/ui/components/studio-uploader.tsx
+++ src/modules/studio/ui/components/studio-uploader.tsx
@@ -1,4 +1,13 @@
-import MuxUploader from "@mux/mux-uploader-react";
+import { Button } from "@/components/ui/button";
+import MuxUploader, {
+    MuxUploaderDrop,
+    MuxUploaderFileSelect,
+    MuxUploaderProgress,
+    MuxUploaderStatus,
+} from "@mux/mux-uploader-react";
+import { Upload } from "lucide-react";
+
+const UPLOADER_ID = "video-uploader";
 
 interface StudioUploaderProps {
     endpoint?: string | null;
@@ -11,7 +20,46 @@ export const StudioUploader = ({
 }: StudioUploaderProps) => {
     return (
         <div>
-            <MuxUploader endpoint={endpoint} onSuccess={onSuccess} />
+            <MuxUploader
+                onSuccess={onSuccess}
+                endpoint={endpoint}
+                id={UPLOADER_ID}
+                className="hidden group/hover"
+            />
+            <MuxUploaderDrop muxUploader={UPLOADER_ID} className="group/drop">
+                <div
+                    className="flex flex-col items-center gap-6"
+                    slot="heading"
+                >
+                    <div className="flex items-center justify-center gap-2 rounded-full bg-muted h-32 w-32">
+                        <Upload className="size-10 text-muted-foreground group/drop-[&[active]]:animate-bounce" />
+                    </div>
+                    <div className="flex flex-col gap-2 text-center">
+                        <p className="text-sm">
+                            Drag & drop video files to upload
+                        </p>
+                        <p className="text-xs text-muted-foreground">
+                            Your videos will be private until you publish them
+                        </p>
+                    </div>
+                    <MuxUploaderFileSelect muxUploader={UPLOADER_ID}>
+                        <Button type="button" className="rounded-full">
+                            Select files
+                        </Button>
+                    </MuxUploaderFileSelect>
+                </div>
+                <span slot="separator" className="hidden" />
+                <MuxUploaderStatus
+                    muxUploader={UPLOADER_ID}
+                    className="text-sm"
+                />
+                <MuxUploaderProgress
+                    muxUploader={UPLOADER_ID}
+                    className="text-sm"
+                    type="percentage"
+                />
+                <MuxUploaderProgress muxUploader={UPLOADER_ID} type="bar" />
+            </MuxUploaderDrop>
         </div>
     );
 };
